3a86c59558390048a6943f16a44968b21590bba3
necmigunduz <necmigunduz@gmail.com>
2021-08-04 15:39:34 +0300

    Deactivate creat, update and destroy methods in units controllers and do the relevant revisions in routes and request test

--- spec/requests/units_spec.rb
+++ spec/requests/units_spec.rb
@@ -57,62 +57,62 @@ RSpec.describe 'units API', type: :request do
   end
 
   # Test suite for POST /units
-  describe 'POST /units' do
-    # valid payload
-    let(:valid_attributes) do
-      { title: 'Learn Elm' }.to_json
-    end
+  # describe 'POST /units' do
+  #   # valid payload
+  #   let(:valid_attributes) do
+  #     { title: 'Learn Elm' }.to_json
+  #   end
 
-    context 'when the request is valid' do
-      before { post '/units', params: valid_attributes, headers: headers }
+  #   context 'when the request is valid' do
+  #     before { post '/units', params: valid_attributes, headers: headers }
 
-      it 'creates a unit' do
-        expect(json['title']).to eq('Learn Elm')
-      end
+  #     it 'creates a unit' do
+  #       expect(json['title']).to eq('Learn Elm')
+  #     end
 
-      it 'returns status code 201' do
-        expect(response).to have_http_status(200)
-      end
-    end
+  #     it 'returns status code 201' do
+  #       expect(response).to have_http_status(200)
+  #     end
+  #   end
 
-    context 'when the request is invalid' do
-      let(:invalid_attributes) { { title: nil }.to_json }
-      before { post '/units', params: invalid_attributes, headers: headers }
+  #   context 'when the request is invalid' do
+  #     let(:invalid_attributes) { { title: nil }.to_json }
+  #     before { post '/units', params: invalid_attributes, headers: headers }
 
-      it 'returns status code 422' do
-        expect(response).to have_http_status(422)
-      end
+  #     it 'returns status code 422' do
+  #       expect(response).to have_http_status(422)
+  #     end
 
-      it 'returns a validation failure message' do
-        expect(json['message'])
-          .to match(/Validation failed: Title can't be blank/)
-      end
-    end
-  end
+  #     it 'returns a validation failure message' do
+  #       expect(json['message'])
+  #         .to match(/Validation failed: Title can't be blank/)
+  #     end
+  #   end
+  # end
 
   # Test suite for PUT /units/:id
-  describe 'PUT /units/:id' do
-    let(:valid_attributes) { { title: 'Shopping' }.to_json }
+  # describe 'PUT /units/:id' do
+  #   let(:valid_attributes) { { title: 'Shopping' }.to_json }
 
-    context 'when the record exists' do
-      before { put "/units/#{unit_id}", params: valid_attributes, headers: headers }
+  #   context 'when the record exists' do
+  #     before { put "/units/#{unit_id}", params: valid_attributes, headers: headers }
 
-      it 'updates the record' do
-        expect(response.body).to be_empty
-      end
+  #     it 'updates the record' do
+  #       expect(response.body).to be_empty
+  #     end
 
-      it 'returns status code 204' do
-        expect(response).to have_http_status(204)
-      end
-    end
-  end
+  #     it 'returns status code 204' do
+  #       expect(response).to have_http_status(204)
+  #     end
+  #   end
+  # end
 
   # Test suite for DELETE /units/:id
-  describe 'DELETE /units/:id' do
-    before { delete "/units/#{unit_id}", params: {}, headers: headers }
+  # describe 'DELETE /units/:id' do
+  #   before { delete "/units/#{unit_id}", params: {}, headers: headers }
 
-    it 'returns status code 204' do
-      expect(response).to have_http_status(204)
-    end
-  end
+  #   it 'returns status code 204' do
+  #     expect(response).to have_http_status(204)
+  #   end
+  # end
 end
