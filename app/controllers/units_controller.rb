26ffd8fe91646a8c837b969212745ce1d624b52f
necmigunduz <necmigunduz@gmail.com>
2021-08-04 14:15:24 +0300

    Return to previous units controller settings

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -1,21 +1,43 @@
 # app/controllers/units_controller.rb
 class UnitsController < ApplicationController
-    before_action :set_unit, only: [:show]
-  
-    # GET /units
-    def index
-      @units = Unit.all
-      json_response(@units)
-    end
-  
-    # GET /units/:id
-    def show
-      json_response(@unit)
-    end
-  
-    private
-  
-    def set_unit
-      @unit = Unit.find(params[:id])
-    end
+  before_action :set_unit, only: [:show, :update, :destroy]
+
+  # GET /units
+  def index
+    @units = current_user.measurements
+    json_response(@units)
   end
+
+  # POST /units
+  def create
+    @unit = Unit.create!(unit_params)
+    json_response(@unit)
+  end
+
+  # GET /units/:id
+  def show
+    json_response(@unit)
+  end
+
+  # PUT /units/:id
+  def update
+    @unit.update(unit_params)
+    head :no_content
+  end
+
+  # DELETE /units/:id
+  def destroy
+    @unit.destroy
+    head :no_content
+  end
+
+  private
+
+  def unit_params
+    params.permit(:title)
+  end
+
+  def set_unit
+    @unit = Unit.find(params[:id])
+  end
+end
