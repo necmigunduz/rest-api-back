18eddbb0ca18f134c4dfbb38ac2b3b322c1890a4
necmigunduz <necmigunduz@gmail.com>
2021-08-15 20:11:17 +0300

    Add default to value

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_14_105246) do
+ActiveRecord::Schema.define(version: 2021_08_15_170833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2021_08_14_105246) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
-    t.decimal "value", precision: 6, scale: 2, null: false
+    t.float "value", default: 0.0, null: false
   end
 
   create_table "units", force: :cascade do |t|
